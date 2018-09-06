86d08cb76d71c52a7db214e6d112ca6caacd3bf2
guohao <37768861+ScDadaguo@users.noreply.github.com>
2018-09-06 12:25:22 +0800

    1
    1

---
+++ vue-playlist/src/components/chinaMoment/Moment1.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="Moment1">
+       
+        <div class="container">
+             <Broadcast></Broadcast>
+            <div class="card" >
+                <div class="card-body" >
+                    <h5 class="card-title">Card title</h5>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=a7458cac4090f60310bd9415587bd87e/0dd7912397dda14480f36c2fb8b7d0a20df486b0.jpg" alt="Card image cap">
+                            </div>
+                        </div>
+                         <div class="col-md-8">
+                             <div class="card" >aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+  
+                         </div>
+                    </div>
+                    
+                </div>
+         </div>
+
+
+         <div class="card" >
+                <div class="card-body" >
+                    <h5 class="card-title">Card title</h5>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=a7458cac4090f60310bd9415587bd87e/0dd7912397dda14480f36c2fb8b7d0a20df486b0.jpg" alt="Card image cap">
+                            </div>
+                        </div>
+                         <div class="col-md-8">
+                             <div class="card" >aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+  
+                         </div>
+                    </div>
+                    
+                </div>
+         </div>
+
+
+         <div class="card" >
+                <div class="card-body" >
+                    <h5 class="card-title">Card title</h5>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=a7458cac4090f60310bd9415587bd87e/0dd7912397dda14480f36c2fb8b7d0a20df486b0.jpg" alt="Card image cap">
+                            </div>
+                        </div>
+                         <div class="col-md-8">
+                             <div class="card" >aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+  
+                         </div>
+                    </div>
+                    
+                </div>
+         </div>
+
+         
+        </div>
+    
+    </div>
+</template>
+
+<script>
+import Broadcast from "../HOME/Broadcast";
+export default {
+  components: {
+    Broadcast: Broadcast
+  },
+  name: "Moment1"
+};
+</script>
+<style coped>
+.card {
+  width: 100%;
+}
+</style>
+
