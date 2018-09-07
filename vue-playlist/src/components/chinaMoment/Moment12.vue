c9848a2bc70b00abf329ad235c73ef31c2c74301
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-09-07 15:17:48 +0800

    add shot

--- vue-playlist/src/components/chinaMoment/Moment12.vue
+++ vue-playlist/src/components/chinaMoment/Moment12.vue
@@ -108,3 +108,129 @@ export default {
 
 </style>
 
+<template>
+    <div class="Moment1">
+
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <div>
+                        <div id="demo" class="carousel slide" data-ride="carousel">
+
+                            <!-- 指示符 -->
+                            <ul class="carousel-indicators">
+                                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                                <li data-target="#demo" data-slide-to="1"></li>
+
+                            </ul>
+
+                            <!-- 轮播图片 -->
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+
+                                    <img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3323254629,294956374&fm=173&app=25&f=JPEG?w=640&h=438&s=3587CAB26A5317C65C1FC7A903006009">
+                                    <div class="carousel-caption">
+                                        <!-- <h3>第一张图片描述标题</h3>
+        <p>描述文字!</p> -->
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+
+                                    <img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3758433204,2506854395&fm=173&app=25&f=JPEG?w=600&h=385&s=E34060A653A216A62FA8308D03005082">
+                                    <div class="carousel-caption">
+                                        <!-- <h3>第二张图片描述标题</h3>
+        <p>描述文字!</p> -->
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <!-- 左右切换按钮 -->
+                            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                            </a>
+                            <a class="carousel-control-next" href="#demo" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="barbar">
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">邹市明奥运夺冠10周年，2夺亚运会拳击金牌，霸气中国第一拳王</h5>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1911758597,171147246&fm=173&app=25&f=JPEG?w=550&h=351&s=F10248B65A72308C5C0604650300506E" alt="Card image cap">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card">
+                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2008年8月24日，在北京工人体育馆拳击赛场男子拳击48KG决赛中，2007年世锦赛冠军邹市明，迎来老对手蒙古的普列布道尔吉-塞尔丹巴。比赛中，邹市明凭借“海盗式”打法，以1-0击败对手夺得冠军。</p>
+                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;这个冠军不仅是邹市明本人和中国拳击队的首枚金牌，同时也是中国代表团的第50枚金牌。邹市明夺冠之后，北京工人体育馆成为欢乐的海洋，现场观众高呼邹市明的名字，并齐声高唱中华人民共和国国歌……。</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">尹军花卫冕亚运拳击冠军</h5>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="http://cms-bucket.nosdn.127.net/2018/09/01/757eae4640e4472fbdf9b59e10dde5d8.jpeg?imageView&thumbnail=550x0" alt="Card image cap">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card">
+                                <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;北京时间9月1日，雅加达亚运会结束拳击项目的争夺。女子57公斤级决赛，中国选手尹军花4-1战胜朝鲜选手周孙和，夺得了中国队在本届亚运会拳击项目的第二枚金牌。</p>
+                                <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在亚洲赛场，尹军花打法先进，其速度和击打准确度让她在亚洲拳坛独树一帜。四年前的韩国仁川亚运会，中国拳击队获得了1金2银，其中的唯一一金就来自与尹军花所在的女子60公斤级。</p>
+
+                                <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;本届亚运会，尹军花延续强势，一路打进决赛。面对朝鲜选手周孙和的挑战，尹军花在短暂的慢热后占据主动。特别是来到第二局，她依靠节奏和移动上的改变牵制对手，反观周孙和则连续被击中。来到第三局，尹军花越战越勇，在犀利的攻势之下，周孙和已经无力回天。</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+import Broadcast from "../HOME/Broadcast";
+export default {
+    components: {
+        Broadcast: Broadcast
+    },
+    name: "Moment1"
+};
+</script>
+<style scoped>
+.card {
+    width: 100%;
+}
+.Moment1 .carousel-item img {
+    width: 100%;
+    height: 550px;
+}
+</style>
+
--- vue-playlist/src/components/chinaMoment/Moment16.vue
+++ vue-playlist/src/components/chinaMoment/Moment16.vue
@@ -1,149 +1,103 @@
 <template>
     <div class="Moment9">
-       
+
         <div class="container">
-             <div>
-	<div id="demo" class="carousel slide" data-ride="carousel">
- 
-  <!-- 指示符 -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
- 
-  <!-- 轮播图片 -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../../assets/moment/pihuatinglunbo1.jpg">
-      <div class="carousel-caption">
-        <h3></h3>
-        
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="../../assets/moment/pihuatinglunbo2.jpg">
-      <div class="carousel-caption">
-        <h3></h3>
-       
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="../../assets/moment/pihuatinglunbo3.jpg">
-      <div class="carousel-caption">
-        <h3></h3>
-        
-      </div>
-    </div>
-  </div>
- 
-  <!-- 左右切换按钮 -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
-	</div>
-  <div class="barbar">
-    
-  </div>
-</div>
-            <div class="card" >
-                <div class="card-body" >
-                    <h5 class="card-title">卜廷凯和张冬雅加达亚运会夺得双人1000米皮划艇冠军</h5>
+            <div>
+                <div id="demo" class="carousel slide" data-ride="carousel">
+
+                    <!-- 指示符 -->
+                    <ul class="carousel-indicators">
+                        <li data-target="#demo" data-slide-to="0" class="active"></li>
+                        <li data-target="#demo" data-slide-to="1"></li>
+                        <li data-target="#demo" data-slide-to="2"></li>
+                    </ul>
+
+                    <!-- 轮播图片 -->
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2931361240,1677280544&fm=173&app=25&f=JPEG?w=640&h=432&s=9AA24483844307454C14E1B00300C001">
+                            <div class="carousel-caption">
+                                <h3></h3>
+
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3475729050,2140346660&fm=173&app=25&f=JPEG?w=640&h=432&s=DB9008C44417D5D64884B5B803001001">
+                            <div class="carousel-caption">
+                                <h3></h3>
+
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=4102002890,363729179&fm=173&app=25&f=JPEG?w=640&h=445&s=D6A20CE48472F1D60A8DD3B80300C002">
+                            <div class="carousel-caption">
+                                <h3></h3>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- 左右切换按钮 -->
+                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#demo" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
+                </div>
+                <div class="barbar">
+
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">张心妍拿下亚运会中国射箭女子第一金</h5>
                     <div class="row">
                         <div class="col-md-4">
                             <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="../../assets/moment/pihuating1.jpg" alt="Card image cap">
+                                <img class="card-img-top" src="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=4102002890,363729179&fm=173&app=25&f=JPEG?w=640&h=445&s=D6A20CE48472F1D60A8DD3B80300C002" alt="Card image cap">
                             </div>
                         </div>
-                         <div class="col-md-8">
-                             <div class="card" >
-                                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>8月30日</strong>，2018年雅加达亚运会皮划艇静水比赛决赛在巨港体育中心展开。辛寨小伙儿张冬在男子双人皮艇1000米决赛冲刺阶段与队友成功完成反超，最终以3分23秒601的成绩获得金牌，这也是中国队时隔八年再夺该项目桂冠。</p>
-                                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>2018雅加达亚运会</strong>，中国队在男子双人皮艇1000米比赛派出了卜廷凯和张冬出战。预赛中，卜廷凯和张冬小组第一，总排名第二晋级。决赛过程竞争异常激烈，最后250米，卜廷凯/张冬奋力一搏，在最后关头完成超越，最终以3分23秒601的成绩夺得金牌。</p>
-                                
-                            
-                            </div>
-  
-                         </div>
-                    </div>
-                    
-                </div>
-         </div>
+                        <div class="col-md-8">
+                            <div class="card">
+                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <strong>8月28日</strong>报道，世界排名第113位的中国选手张心妍，进入射箭女子个人反曲弓决赛与东道主选手蒂安纳达争夺冠军。 最终张心妍发挥出色以7-3获胜，赢得中国射箭本届亚运会的第一枚金牌。张心妍由此成为自1978年射箭成为亚运会正式比赛项目以来，第一位夺得女子个人赛的金牌的中国选手。
+                                </p>
+                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;据澎湃新闻报道，对于这一成绩，前奥运冠军、上海市射击射箭运动中心副主任陶璐娜表示，“虽然就像我们的国球一样，韩国是将射箭作为举国性的、招牌性的项目。但现在成绩证明，我们中国也不差。”。</p>
 
-
-         <div class="card" >
-                <div class="card-body" >
-                    <h5 class="card-title">马亚男、孙梦雅获雅加达亚运会皮划艇静水双人500米冠军</h5>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="../../assets/moment/pihuating2.jpg" alt="Card image cap">
                             </div>
+
                         </div>
-                         <div class="col-md-8">
-                             <div class="card" >
-                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>8月30日</strong>，2018年雅加达亚运会皮划艇静水比赛在巨港体育中心展开第二日争夺。运动员马亚男和队友孙梦雅出战女子双人划艇500米，并在决赛中发挥出色，以2分02秒512的成绩夺得冠军，为中国代表团争得了荣誉。</p>
-                               <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>2018年雅加达亚运会</strong>皮划艇静水决赛日全面展开，中国组合-马亚男/孙梦雅在女子双人划艇500米决赛，最终历时2分02秒512的成绩成功夺金。中国搭档-马亚男/孙梦雅是被分在A组决赛第2道，从第2道起步划桨节奏紧凑，加速到250米处耗时59秒32，最终，以2分02秒512的成绩率先撞线斩获冠军</p>
-                             </div>
-  
-                         </div>
                     </div>
-                    
+
                 </div>
-         </div>
+            </div>
 
-
-         <div class="card" >
-                <div class="card-body" >
-                    <h5 class="card-title">全鑫在雅加达亚运会勇夺皮划艇激流回旋男子单人皮艇冠军</h5>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="../../assets/moment/pihuatingquanxin.jpg" alt="Card image cap">
-                            </div>
-                        </div>
-                         <div class="col-md-8">
-                             <div class="card" >
-                                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>8月23日</strong>，全鑫在2018年雅加达亚运会皮划艇激流回旋比赛男子单人皮艇决赛中，发挥出色，夺得金牌。这是中国队继广州亚运会后，时隔八年再夺该项目冠军。</p>
-                                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在当天的比赛中，全鑫在预赛就表现出了良好的状态，以81秒26排名第一，顺利晋级。半决赛，全鑫继续强势，他以84秒12名列第一，闯进了决赛。决赛中，全鑫的状态非常好，最终以89秒39夺得冠军，夺得了个人首枚亚运会金牌。</p>
-                                 
-                             </div>
-  
-                         </div>
-                    </div>
-                    
-                </div>
-         </div>
-
-         
         </div>
-    
+
     </div>
 </template>
 
 <script>
 export default {
-  name: "Momen9",
-  created(){
-      this.menu();
-  },
-  methods:{
-      menu() {
-         window.scrollTo(0,0);
+    name: "Momen9",
+    created() {
+        this.menu();
     },
-  }
+    methods: {
+        menu() {
+            window.scrollTo(0, 0);
+        }
+    }
 };
 </script>
 <style coped>
 .card {
-  width: 100%;
+    width: 100%;
 }
-.Moment9
-.carousel-item img{
-  width: 100%;
-  height: 550px;
+.Moment9 .carousel-item img {
+    width: 100%;
+    height: 550px;
 }
 </style>
 
