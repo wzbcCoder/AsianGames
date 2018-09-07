c5b1d21adb69dd867d017f08194efd0fc7a4d289
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-09-07 15:47:13 +0800

    add video

--- vue-playlist/src/components/HOME/Video.vue
+++ vue-playlist/src/components/HOME/Video.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="vd">
     <video id="my-video" class="video-js" controls preload="auto" width="100%" height="100%"
-        poster="../../assets/logo.png" autoplay data-setup="{}">
-        <source src="../../../static/kebi.mp4" type='video/mp4'>
+         autoplay data-setup="{}">
+        <source src="../../../static/shouyevideo.mp4" type='video/mp4'>
         <p class="vjs-no-js">
             To view this video please enable JavaScript, and consider upgrading to a web browser that
         <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
