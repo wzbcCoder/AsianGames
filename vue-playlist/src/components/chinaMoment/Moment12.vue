77d7420beef2f94cbacdd3f3b8f55ca6883c8377
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-09-07 13:23:48 +0800

    add fencing

---
+++ vue-playlist/src/components/chinaMoment/Moment12.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="Moment1">
+       
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                      <div>
+	<div id="demo" class="carousel slide" data-ride="carousel">
+ 
+  <!-- 指示符 -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+   
+  </ul>
+ 
+  <!-- 轮播图片 -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        
+      <img src="http://5b0988e595225.cdn.sohucs.com/images/20180825/eec69c3c998f4f3c9d9e32818ba0e8b3.jpeg">
+      <div class="carousel-caption">
+        <!-- <h3>第一张图片描述标题</h3>
+        <p>描述文字!</p> -->
+      </div>
+    </div>
+    <div class="carousel-item">
+        
+      <img src="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3978561341,380822833&fm=173&app=25&f=JPEG?w=581&h=325&s=61512B66C68584A42124E99A0300C092">
+      <div class="carousel-caption">
+        <!-- <h3>第二张图片描述标题</h3>
+        <p>描述文字!</p> -->
+      </div>
+    </div>
+    
+  </div>
+ 
+  <!-- 左右切换按钮 -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+	</div>
+ 
+</div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="barbar">
+            </div>
+                </div>
+            </div>
+             
+
+           
+            <div class="card" >
+                <div class="card-body" >
+                    <h5 class="card-title">孙一文一剑致胜 中国队卫冕亚运会女子重剑团体赛金牌</h5>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="http://05imgmini.eastday.com/mobile/20180824/20180824235353_d3bb45c72124f8aeb8c32cb4f1134b3a_1.jpeg" alt="Card image cap">
+                            </div>
+                        </div>
+                         <div class="col-md-8">
+                             <div class="card" >
+                                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;中新网雅加达 8 月 24 日电 ( 记者 岳川 ) 24 日，雅加达亚运会击剑项目进入最后比赛日。在女子重剑团体决赛中，中国队通过加赛 29：28 击败韩国队，孙一文一剑致胜。</p>
+                                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;作为亚运会该项目卫冕冠军，女子重剑团体是中国击剑的优势项目。中国队不仅在 2012 年伦敦奥运会中折桂，2006 年和 2015 年还两夺世锦赛冠军。在今年 7 月的无锡世锦赛上，由许诚子、朱明叶、林声、孙一文组成的中国队获团体铜牌。</p>
+                                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;加赛中，韩国队一方的信号灯率先亮起，但崔英杰因膝盖接触剑道而被判犯规在先，并被裁判出示黄牌警告。重新开赛后，孙一文主动出击命中，中国队以 29:28 获得冠军，成功卫冕。</p>
+                            </div>
+  
+                         </div>
+                    </div>
+                    
+                </div>
+         </div>
+
+
+        
+
+         
+        </div>
+    
+    </div>
+</template>
+
+<script>
+import Broadcast from "../HOME/Broadcast";
+export default {
+  components: {
+    Broadcast: Broadcast
+  },
+  name: "Moment1"
+};
+</script>
+<style scoped>
+.card {
+  width: 100%;
+}
+.Moment1
+.carousel-item img{
+  width: 100%;
+  height: 550px;
+}
+
+</style>
+
